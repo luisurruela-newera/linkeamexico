f52386f9662e9993a5e4c63c10abfa36e434f098
Luis U. <luis.urruela@neweratech.com>
2025-05-04 18:20:02 -0600

    Updated config for github pages deploy to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://luisurruela-newera.github.io",
+  base: "/linkeamexico/",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [icon()]
+  integrations: [icon()],
 });
