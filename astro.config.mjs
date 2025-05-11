6f7af155481d9423fa1850981953aa3556700b32
Luis U. <luis.urruela@neweratech.com>
2025-05-11 00:52:39 -0600

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://luisurruela-newera.github.io",
+  site: "https://linkeamexico.netlify.app/",
   output: "server",
 
   vite: {
